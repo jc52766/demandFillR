24665c61e442652789d28811eef90231e1db2108
Jason Corderoy <analytics.jc@gmail.com>
2024-11-13 14:55:04 +1100

    tests added all zeros

--- tests/testthat/test_solver.R
+++ tests/testthat/test_solver.R
@@ -22,3 +22,50 @@ test_that("test basic solve", {
   expect_identical(solved, expect)
   #identical(solved, expect)
 })
+
+
+test_that("test all zero selected", {
+  # Very heavily punish exceed demand so should pick all 0 option at that point
+  solved <- matrix(rep(0, 12), ncol = 3, byrow = T)
+  options <- matrix(c(1, 1, 1, 0, 0, 0), ncol = 3, byrow = T)
+  demand <- c(2, 1, 1)
+  dfw <- c(1, 1, 1)
+  dew <- c(-50, -50, -50)
+  expect <- matrix(c(1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+                   ncol = 3,
+                   byrow = T)
+  for (i in 1:4) {
+    solved <- xsolve(
+      solved = solved,
+      options = options,
+      demand = demand,
+      dfw = dfw,
+      dew = dew
+    )
+  }
+  expect_identical(solved, expect)
+  #identical(solved, expect)
+})
+
+test_that("test all not zero selected", {
+  # Don't punish exceed demand so should not pick all 0 option ever
+  solved <- matrix(rep(0, 12), ncol = 3, byrow = T)
+  options <- matrix(c(1, 1, 1, 0, 0, 0), ncol = 3, byrow = T)
+  demand <- c(2, 1, 1)
+  dfw <- c(0.1, 0.1, 0.1)
+  dew <- c(1, 1, 1)
+  expect <- matrix(c(1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1),
+                   ncol = 3,
+                   byrow = T)
+  for (i in 1:4) {
+    solved <- xsolve(
+      solved = solved,
+      options = options,
+      demand = demand,
+      dfw = dfw,
+      dew = dew
+    )
+  }
+  expect_identical(solved, expect)
+  #identical(solved, expect)
+})
