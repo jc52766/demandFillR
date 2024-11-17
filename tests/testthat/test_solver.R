cabdb2a22bd20116de01445d314e08ab87fececf
Jason Corderoy <analytics.jc@gmail.com>
2024-11-18 10:14:08 +1100

    multiple primary drop test case in

--- tests/testthat/test_solver.R
+++ tests/testthat/test_solver.R
@@ -123,3 +123,57 @@ test_that("test that can improve with more solves", {
   }
   expect_identical(solved, expect2)
 })
+
+test_that("test multiple primary drops", {
+  solved <- matrix(rep(0, 12), ncol = 3, byrow = T)
+  options1 <- matrix(c(1, 0, 0, 0.5, 0.5, 0), ncol = 3, byrow = T)
+  options2 <- matrix(c(0, 1, 0, 0, 0.5, 0.5), ncol = 3, byrow = T)
+  options3 <- matrix(c(0, 0, 1, 0, 0.5, 0.5), ncol = 3, byrow = T)
+  options_l <- list(options1, options2, options3)
+  demand <- c(1, 1, 1)
+  dfw <- c(1, 1, 1)
+  dew <- c(-0.5, -0.5, -0.5)
+  expect_l <- list(
+    matrix(c(1, 0, 0),
+                   ncol = 3,
+                   byrow = T),
+    matrix(c(0, 1, 0),
+                    ncol = 3,
+                    byrow = T),
+    matrix(c(0, 0, 1),
+                    ncol = 3,
+                    byrow = T)
+  )
+  
+  expect_final_solve <- matrix(c(rep(0,9), c(1, 1, 1)),
+                               ncol = 3,
+                               byrow = T)
+  
+  solves_l <- list()
+  original_solved <- solved
+  for (i in 1:3) {
+    this_opt <- options_l[[i]]
+    solve1 <- xsolve(
+      solved = solved,
+      options = this_opt,
+      demand = demand,
+      dfw = dfw,
+      dew = dew
+    )
+    # store this solve
+    this_solve <- solve1 %>% tail(1) %>% matrix(ncol=3)
+    solves_l[[i]] <- this_solve
+    
+    # temporarily update solved to row bind this primary drops solve
+    solved <- rbind(solved, this_solve)
+    
+    expect_identical(solves_l[[i]], expect_l[[i]])
+  }
+  allSolve <- do.call(rbind,solves_l) %>% rowSums()
+  
+  # now behead original_solved and row bind the solve across all primary drops
+  beheaded_original_solved <- original_solved[-1, ]
+  final_solve <- beheaded_original_solved %>% rbind(allSolve) %>% matrix(ncol=3)
+  
+  expect_identical(final_solve, expect_final_solve)
+})
